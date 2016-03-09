dbfb411c33105173ab2455445a06240a9dbd27a9
GDmail <GDmail91@gmail.com>
2016-03-09 22:14:08 +0900

    구글 API 추가 및 슬라이딩 패널 추가

--- app/src/main/java/org/daelimie/test/daelimie/AddAlram.java
+++ app/src/main/java/org/daelimie/test/daelimie/AddAlram.java
@@ -1,8 +1,11 @@
 package org.daelimie.test.daelimie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by YS on 2016-03-09.
@@ -16,13 +19,14 @@ public class AddAlram extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         // 클릭키를 추가할 버튼정의
-        //Button addBtn = (Button) findViewById(R.id.addRouteButton);
-        /*addBtn.setOnClickListener(new View.OnClickListener() {
+        Button setDeparture = (Button) findViewById(R.id.setDeparture);
+        setDeparture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO 경로 추가 버튼
+                Intent intent = new Intent(AddAlram.this, LocatePicker.class);
+                startActivity(intent);
             }
         });
-*/
+
     }
 }
---
+++ app/src/main/java/org/daelimie/test/daelimie/LocatePicker.java
@@ -0,0 +1,121 @@
+package org.daelimie.test.daelimie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by YS on 2016-03-09.
+ */
+public class LocatePicker extends AppCompatActivity {
+
+    private static final String TAG = "LocatePicker";
+
+    static final LatLng SEOUL = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
+
+    private SlidingUpPanelLayout mLayout;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.locate_picker);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // 구글맵 컴포넌트
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        Marker seoul = map.addMarker(new MarkerOptions().position(SEOUL)
+                .title("Seoul"));
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(SEOUL, 15));
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+
+
+        // 리사이클러 뷰
+        ListView lv = (ListView) findViewById(R.id.list);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(LocatePicker.this, "onItemClick", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        List<String> your_array_list = Arrays.asList(
+                "This",
+                "Is",
+                "An",
+                "Example",
+                "ListView",
+                "That",
+                "You",
+                "Can",
+                "Scroll",
+                ".",
+                "It",
+                "Shows",
+                "How",
+                "Any",
+                "Scrollable",
+                "View",
+                "Can",
+                "Be",
+                "Included",
+                "As",
+                "A",
+                "Child",
+                "Of",
+                "SlidingUpPanelLayout"
+        );
+
+        // This is the array adapter, it takes the context of the activity as a
+        // first parameter, the type of list view as a second parameter and your
+        // array as a third parameter.
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                your_array_list );
+
+        lv.setAdapter(arrayAdapter);
+
+        mLayout = (SlidingUpPanelLayout) findViewById(R.id.sliding_layout);
+        mLayout.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
+            @Override
+            public void onPanelSlide(View panel, float slideOffset) {
+                Log.i(TAG, "onPanelSlide, offset " + slideOffset);
+            }
+
+            @Override
+            public void onPanelStateChanged(View panel, SlidingUpPanelLayout.PanelState previousState, SlidingUpPanelLayout.PanelState newState) {
+                Log.i(TAG, "onPanelStateChanged " + newState);
+            }
+        });
+        mLayout.setFadeOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mLayout.setPanelState(SlidingUpPanelLayout.PanelState.COLLAPSED);
+            }
+        });
+
+    }
+}
